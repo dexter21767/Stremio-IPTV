9c324a12477caace4d8c11c41aea95f866f909fe
dexter21767 <ahmidiyasser@egmail.com>
2022-09-05 11:15:45 +0100

    fix port

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = require('./index.js')
 
 // create local server
 app.listen((process.env.PORT || 63355), function () {
-    console.log('Addon active on port 63355.');
-    console.log('HTTP addon accessible at: http://127.0.0.1:63355/manifest.json');
+    console.log(`Addon active on port ${process.env.PORT || 63355} .`);
+    console.log(`HTTP addon accessible at: http://127.0.0.1:${process.env.PORT || 63355}/manifest.json`);
 });
