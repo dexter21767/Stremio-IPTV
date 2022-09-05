68511764c6418f010eb8ce88358f2621d4615482
dexter21767 <ahmidiyasser@egmail.com>
2022-09-05 11:10:31 +0100

    Auto-commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const app = require('./index.js')
 
 // create local server
-app.listen(7515, function () {
-    console.log('Addon active on port 7515.');
-    console.log('HTTP addon accessible at: http://127.0.0.1:7515/manifest.json');
+app.listen((process.env.PORT || 63355), function () {
+    console.log('Addon active on port 63355.');
+    console.log('HTTP addon accessible at: http://127.0.0.1:63355/manifest.json');
 });
