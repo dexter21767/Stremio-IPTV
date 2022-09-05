88d32fb02160c77428cc40d696c51ddc914a1c19
dexter21767 <ahmidiyasser@egmail.com>
2022-09-05 11:17:51 +0100

    fix port

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 // require serverless version
 const app = require('./index.js')
+const { serveHTTP, publishToCentral } = require("stremio-addon-sdk")
 
 // create local server
 app.listen((process.env.PORT || 63355), function () {
     console.log(`Addon active on port ${process.env.PORT || 63355} .`);
     console.log(`HTTP addon accessible at: http://127.0.0.1:${process.env.PORT || 63355}/manifest.json`);
 });
+
+publishToCentral("https://2ecbbd610840-stremio-iptv.baby-beamup.club/manifest.json");
